febc5dfe636a27117bb1354f9bc1df1ef29f1442
Your Name <you@example.com>
2018-08-03 20:39:39 +0600

    more variables

--- Page23.sql
+++ Page23.sql
@@ -1,12 +1,13 @@
 Declare
 v_helloWorld varchar2(20):='Hello Everybody';
-
+v_a_number number(10,2):=400.4599;
+v_aa_number number(10,4):=400.4599;
 
 Begin
 
 dbms_output.put_line(v_helloWorld);
-
-
+dbms_output.put_line(v_a_number);
+dbms_output.put_line(v_aa_number);
 
 
 
