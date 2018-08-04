5e00b9f530c044ee5f14e1a2648e2fceb5867501
Your Name <you@example.com>
2018-08-04 23:32:56 +0600

    basic loop practise

---
+++ basicLoop.sql
@@ -0,0 +1,16 @@
+DECLARE
+x number := 10;
+BEGIN
+LOOP
+dbms_output.put_line(x);
+x := x + 10;
+IF 
+(x > 150) 
+THEN
+exit;
+END IF;
+END LOOP;
+-- after exit, control resumes here
+dbms_output.put_line('After Exit x is: ' || x);
+END;
+/
